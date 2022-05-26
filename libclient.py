c83fa36fc8a25b9f7dc14c5c8a1a3c099001619e
yeowhengg <20042741@myrp.edu.sg>
2022-05-26 19:38:52 +0800

    Initial Commit

---
+++ libclient.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 26 19:37:57 2022
+
+@author: YeowHeng
+"""
---
+++ libserver.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 26 19:37:46 2022
+
+@author: YeowHeng
+"""
